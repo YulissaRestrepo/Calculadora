571cdcb800b4b3083eb3e3473aacc64e54639a01
Yulissa Restrepo <yulissa.restrepo@utp.ac.pa>
2021-11-02 03:44:03 -0500

    LAB.8 - Desarrollo de pruebas unitarias con PHPUnit - 1SF132

--- app/Calculadora.php
+++ app/Calculadora.php
@@ -1,6 +1,37 @@
 <?php
-class Calculadora{
-    function sumar($num1, $num2){
-        return $num1+$num2;
+class Calculadora
+{
+    function sumar($numero1, $numero2)
+    {
+        return ($numero1 + $numero2);
+    }
+
+    function restar($numero1, $numero2)
+    {
+        return ($numero1 - $numero2);
+    }
+
+    function multiplicar($numero1, $numero2)
+    {
+        return ($numero1 * $numero2);
+    }
+
+    function dividir($numero1, $numero2)
+    {
+        if ($numero2 == 0)
+        {
+            throw new Exception("División por cero");
+        }
+        return ($numero1 / $numero2);
+    }
+
+    function generarArreglo()
+    {
+        $arreglo = array();
+        for($i = 0 ; $i < 5 ; $i++)
+        {
+            $arreglo[$i] = rand(1,10);
+        }
+        return $arreglo;
     }
 }
