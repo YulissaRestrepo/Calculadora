bfdcb4bf81c9c7e09e06b5a2fdcbe3f99d5c31fd
Yulissa Restrepo <yulissa.restrepo@utp.ac.pa>
2021-11-24 04:22:19 -0500

    Laboratorio 9 - PHPUnit doble de Pruebas - 1SF132

--- app/CalculadoraTest.php
+++ app/CalculadoraTest.php
@@ -3,6 +3,9 @@
 require_once(__DIR__.'/Calculadora.php');
 
 use \PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\equalTo;
+
 class CalculadoraTest extends TestCase
 {
 
@@ -100,5 +103,62 @@ class CalculadoraTest extends TestCase
         //$this->assertContains(2, $calculadora->generarArreglo());
         //$this->assertCount(1, $calculadora->generarArreglo());
         $this->assertNotEmpty($calculadora->generarArreglo());
+    }
+
+    public function testCapturarEntradasPermutacion()
+    {
+        // Se crea el doble de prueba para la clase Calculadora, método 'capturarEntradasPermutacion'
+        $stub = $this->createMock('Calculadora');
+        $stub->method('capturarEntradasPermutacion')
+            ->willReturn(array(5, 3));
+
+        $this->assertSame(array(5, 3), $stub->capturarEntradasPermutacion());
+    }
+
+    public function testCalcularPermutacion()
+    {
+        /* Se crea un mock para la clase Calculadora.
+         Solo se hace mock al método calcularFactorial*/
+        $mock = $this->getMockBuilder('Calculadora')
+            ->onlyMethods(array('calcularFactorial'))
+            ->getMock();
+
+/* Se configuran las expectativas para el método calcularFactorial
+se llamará dos veces y devolverá 120 y 6, en cada ocasión, respectivamente. */
+        $mock->expects($this->exactly(2))
+            ->method('calcularFactorial')
+            ->will($this->onConsecutiveCalls(120, 6));
+
+        /* Se hace el assert. */
+        $this->assertSame(20, $mock->calcularPermutacion(5, 2));
+    }
+
+    public function testComprobarLlamada()
+    {
+        $mock = $this->getMockBuilder('Calculadora')
+            ->onlyMethods(array('calcularFactorial'))
+            ->getMock();
+       /* $mock->expects($this->exactly(2))
+            ->method('calcularFactorial')
+            ->withConsecutive([5],[3]);
+        
+        $mock->calcularFactorial(5);
+        $mock->calcularFactorial(4);
+
+        $mock->expects($this->once())
+            ->method('calcularFactorial')
+            ->with(5)
+            ->will($this->returnValue(120)); 
+        $resultado_calculado = $mock->calcularFactorial(5);
+        $this->assertEquals(120, $resultado_calculado);
+        //$mock->calcularFactorial(3);
+        $this->assertEquals(12, $resultado_calculado); */
+       
+        $mock->expects($this->exactly(2))
+            ->method('calcularFactorial')
+            ->withConsecutive([5],[3])
+            ->will($this->onConsecutiveCalls(120, 6));
+        $this->assertEquals(120, $mock->calcularFactorial(5));
+        $this->assertEquals(6, $mock->calcularFactorial(3));
     }
 }
